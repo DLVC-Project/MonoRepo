bab61c6aafc44a9f778a3cbd7c23f0b89c96bdd7
BoboTheBarbar <tesla@e.mail.de>
2022-10-22 22:58:11 +0200

    feat:

---
+++ dlvc-heroes-ui/src/components/shared/SimpleNumberInput.vue
@@ -0,0 +1,37 @@
+<template>
+ <q-input outlined type="number" :value="value" :label="label" @input="handleInput" >
+    <template v-slot:append>
+      <q-icon name="shield"/>
+    </template>
+  </q-input>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: "",
+    }
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    handleInput($event) {
+      this.value = $event.target.value;
+    },
+  },
+};
+  /* let label: string;
+  let value: number;
+  let input: number;
+
+  function handleInput($event) {
+    this $event.target.value
+  } */
+</script>
